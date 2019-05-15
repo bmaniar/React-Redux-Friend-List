a94563c7799e001720400a55b6c99d1847398b5a
Bhargav Maniar 2 <bhamania0@publicisgroupe.net>
2019-05-15 15:13:00 +0530

    Basic Webpack and react app setup done

---
+++ webpack.base.conf.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+
+    entry: [
+        'react-hot-loader/patch',
+        './src/index.jsx'
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test:  /\.scss$|\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js',
+        globalObject: 'this'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'Friends List',
+            template: './src/index.html'
+        })
+    ],
+
+    devtool: 'source-map',
+
+    devServer: {
+        contentBase: '/dist',
+        hot: true,
+        historyApiFallback: true,
+        open: 'http://localhost:8080'
+    }
+};
